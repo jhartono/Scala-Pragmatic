b51b991eeab201e736a802ea3839abfd4d2faf52
Jeffry Hartono <jeffry.hartono@gmail.com>
2011-08-30 22:35:32 -0500

    Added TicTacToeBoard Class

--- src/com/jhartono/pragmatic/scala/exercise1/TicTacToe.scala
+++ src/com/jhartono/pragmatic/scala/exercise1/TicTacToe.scala
@@ -3,7 +3,13 @@ package com.jhartono.pragmatic.scala.exercise1
 object TicTacToe {
 
   def main(args: Array[String]): Unit = { 
-    println("Tic Tac Toe");
+    val ticTacToeBoard = new TicTacToeBoard()
+    
+    ticTacToeBoard.put('X', (0, 0))
+    ticTacToeBoard.put('X', (1, 1))
+    ticTacToeBoard.put('X', (2, 2))
+    
+    println(ticTacToeBoard.toString())
   }
 
 }
---
+++ src/com/jhartono/pragmatic/scala/exercise1/TicTacToeBoard.scala
@@ -0,0 +1,44 @@
+package com.jhartono.pragmatic.scala.exercise1
+
+class TicTacToeBoard(size: Int) {
+	
+  val board = Array.ofDim[Char](size, size)
+	
+  def this() {
+    this(3)
+  }
+  
+  def put(char: Char, position: (Int, Int)): Boolean = {
+    if (position._1 > size - 1 || position._2 > size - 1) {
+      return false
+    }
+    
+    board(position._1)(position._2) = char
+    return true
+  }
+  
+  def get(position: (Int, Int)): Char = {
+    if (position._1 > size - 1 || position._2 > size - 1) {
+      return ' '
+    }
+    return board(position._1)(position._2)
+  }
+  
+  override def toString(): String = {
+    val builder = new StringBuilder()
+    
+    for (row <- 0 until size) {
+      if (row > 0) {
+        builder.append('\n')
+      }
+      for (column <- 0 until size) {
+        if (board(row)(column) == 0) {
+          builder.append("_")
+        }
+        builder.append(board(row)(column))
+      }
+    }
+    builder.toString()
+  }
+  
+}
